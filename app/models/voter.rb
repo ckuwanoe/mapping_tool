95cb6dc2e3b98417a6d7b0d90bb2dd4e838e7b64
Corey Kuwanoe <eschalon@Airlicious.local>
2010-05-19 16:28:03 -0700

    Fixing email

--- app/models/voter.rb
+++ app/models/voter.rb
@@ -1,2 +1,8 @@
 class Voter < ActiveRecord::Base
+  validates_presence_of     :street_address
+  validates_presence_of     :city
+  validates_presence_of     :name
+  validates_presence_of     :phone_number
+  validates_presence_of     :email
+  validates_presence_of     :date_searched
 end
